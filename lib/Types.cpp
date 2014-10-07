dc46236787fd5c86a98e93b07fd959c19b10ff13
Josh Blum <josh@joshknows.com>
2014-10-07 13:35:54 -0700

    soapy: replace double pair with range struct

---
+++ lib/Types.cpp
@@ -0,0 +1,18 @@
+// Copyright (c) 2014-2014 Josh Blum
+// SPDX-License-Identifier: BSL-1.0
+
+#include <SoapySDR/Types.hpp>
+
+SoapySDR::Range::Range(void):
+    _min(0.0),
+    _max(0.0)
+{
+    return;
+}
+
+SoapySDR::Range::Range(const double minimum, const double maximum):
+    _min(minimum),
+    _max(maximum)
+{
+    return;
+}
