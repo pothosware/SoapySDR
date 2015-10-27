b964b7061d886e72ada54be4c703a5d6930b5806
Josh Blum <josh@joshknows.com>
2015-10-27 01:10:00 -0700

    updated latest for loader work

--- doxygen/latest/search/functions_b.js
+++ doxygen/latest/search/functions_b.js
@@ -1,4 +1,5 @@
 var searchData=
 [
+  ['unloadmodule',['unloadModule',['../namespaceSoapySDR.html#afd1fd337e716c119d441152d077fd807',1,'SoapySDR']]],
   ['unmake',['unmake',['../classSoapySDR_1_1Device.html#a6747bfba4c369c333e0405ebe007ef29',1,'SoapySDR::Device']]]
 ];
