7f8d0c93df28bf8039bcb85b5f08630d0335a2c6
Josh Blum <josh@joshknows.com>
2016-03-14 12:43:20 -0700

    fix soapy device decl for a C compiler

--- lib/DeviceC.cpp
+++ lib/DeviceC.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2014-2015 Josh Blum
+// Copyright (c) 2014-2016 Josh Blum
 // SPDX-License-Identifier: BSL-1.0
 
 #include "TypeHelpers.hpp"
@@ -20,7 +20,7 @@
 /*******************************************************************
  * Simple subclass definition for device
  ******************************************************************/
-struct SoapySDRDevice : SoapySDR::Device {};
+struct SoapySDRDevice_struct : SoapySDR::Device {};
 
 extern "C" {
 
