2492066388818ad187483316c3d6df8e8ca28754
Josh Blum <josh@joshknows.com>
2016-06-23 09:21:33 -0700

    c++11 flags for cmake config

--- cmake/SoapySDRConfig.cmake
+++ cmake/SoapySDRConfig.cmake
@@ -63,7 +63,17 @@ endif()
 ########################################################################
 # Helpful compiler flags
 ########################################################################
+
+#C++11 is a required language feature for this project
+set(CMAKE_CXX_STANDARD 11)
+
 if(CMAKE_COMPILER_IS_GNUCXX)
+
+    #enable C++11 on older versions of cmake
+    if (CMAKE_VERSION VERSION_LESS "3.1")
+        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++0x")
+    endif()
+
     #force a compile-time error when symbols are missing
     set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -Wl,--no-undefined")
     set(CMAKE_MODULE_LINKER_FLAGS "${CMAKE_MODULE_LINKER_FLAGS} -Wl,--no-undefined")
@@ -84,11 +94,24 @@ if(APPLE)
 endif()
 
 if(MSVC)
+    #C++11 is a required language feature for this project
+    if (${MSVC_VERSION} LESS 1700)
+        message(FATAL_ERROR "the build requires MSVC 2012 or newer for C++11 support")
+    endif()
+
+    #we always want to use multiple cores for compilation
+    add_compile_options(/MP)
+
     #suppress the following warnings which are commonly caused by project headers
     add_compile_options(/wd4251) #disable 'identifier' : class 'type' needs to have dll-interface to be used by clients of class 'type2'
     add_compile_options(/wd4503) #'identifier' : decorated name length exceeded, name was truncated
 
+    #projects should be cross-platform and standard stl functions should work
     add_definitions(-DNOMINMAX) #enables std::min and std::max
+endif()
+
+if ("${CMAKE_SYSTEM_NAME}" STREQUAL "FreeBSD")
+    add_compile_options(-stdlib=libc++)
 endif()
 
 ########################################################################
