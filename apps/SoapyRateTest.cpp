60b8c73a35ea5b1930ac464032b16147f5f6a83b
Josh Blum <josh@joshknows.com>
2016-10-23 00:17:04 -0700

    working rate test

--- apps/SoapyRateTest.cpp
+++ apps/SoapyRateTest.cpp
@@ -2,11 +2,15 @@
 // SPDX-License-Identifier: BSL-1.0
 
 #include <SoapySDR/Device.hpp>
+#include <SoapySDR/Formats.hpp>
+#include <SoapySDR/Errors.hpp>
 #include <string>
 #include <cstdlib>
 #include <iostream>
 #include <stdexcept>
 #include <csignal>
+#include <chrono>
+#include <cstdio>
 
 static sig_atomic_t loopDone = false;
 void sigIntHandler(const int)
@@ -14,13 +18,65 @@ void sigIntHandler(const int)
     loopDone = true;
 }
 
-void runRateTestStreamLoop(SoapySDR::Device *device, SoapySDR::Stream *stream)
+void runRateTestStreamLoop(
+    SoapySDR::Device *device,
+    SoapySDR::Stream *stream,
+    const int direction,
+    const size_t numChans,
+    const size_t elemSize)
 {
+    //allocate buffers for the stream read/write
+    const size_t numElems = device->getStreamMTU(stream);
+    std::vector<std::vector<char>> buffMem(numChans, std::vector<char>(elemSize*numElems));
+    std::vector<void *> buffs(numChans);
+    for (size_t i = 0; i < numChans; i++) buffs[i] = buffMem[i].data();
+
+    //state collected in this loop
+    unsigned int overflows(0);
+    unsigned long long totalSamples(0);
+    const auto startTime = std::chrono::high_resolution_clock::now();
+    auto timeLastPrint = std::chrono::high_resolution_clock::now();
+
+    std::cout << "Starting stream loop, press Ctrl+C to exit..." << std::endl;
     device->activateStream(stream);
     signal(SIGINT, sigIntHandler);
     while (not loopDone)
     {
-        
+        int ret(0);
+        int flags(0);
+        long long timeNs(0);
+        switch(direction)
+        {
+        case SOAPY_SDR_RX:
+            ret = device->readStream(stream, buffs.data(), numElems, flags, timeNs);
+            break;
+        case SOAPY_SDR_TX:
+            ret = device->writeStream(stream, buffs.data(), numElems, flags, timeNs);
+            break;
+        }
+
+        if (ret == SOAPY_SDR_TIMEOUT) continue;
+        if (ret == SOAPY_SDR_OVERFLOW)
+        {
+            overflows++;
+            continue;
+        }
+        if (ret < 0)
+        {
+            std::cerr << "Unexpected stream error " << SoapySDR::errToStr(ret) << std::endl;
+            break;
+        }
+        totalSamples += ret;
+
+        const auto now = std::chrono::high_resolution_clock::now();
+        if (timeLastPrint + std::chrono::seconds(5) < now)
+        {
+            timeLastPrint = now;
+            const auto timePassed = std::chrono::duration_cast<std::chrono::microseconds>(now - startTime);
+            const auto sampleRate = double(totalSamples)/timePassed.count();
+            printf("%g Msps\t%g Bps\tOverflows %u\n", sampleRate, sampleRate*numChans*elemSize, overflows);
+        }
+
     }
     device->deactivateStream(stream);
 }
@@ -39,8 +95,8 @@ int SoapySDRRateTest(
 
         //parse the direction to the integer enum
         int direction(-1);
-        if (directionStr == "TX" or directionStr == "tx") direction = SOAPY_SDR_TX;
         if (directionStr == "RX" or directionStr == "rx") direction = SOAPY_SDR_RX;
+        if (directionStr == "TX" or directionStr == "tx") direction = SOAPY_SDR_TX;
         if (direction == -1) throw std::invalid_argument("direction not in RX/TX: " + directionStr);
 
         //build channels list, using KwargsFromString is a easy parsing hack
@@ -60,10 +116,15 @@ int SoapySDRRateTest(
         //create the stream, use the native format
         double fullScale(0.0);
         const auto format = device->getNativeStreamFormat(direction, channels.front(), fullScale);
+        const size_t elemSize = SoapySDR::formatToSize(format);
         auto stream = device->setupStream(direction, format, channels);
 
         //run the rate test one setup is complete
-        runRateTestStreamLoop(device, stream);
+        std::cout << "Stream format: " << format << std::endl;
+        std::cout << "Num channels: " << channels.size() << std::endl;
+        std::cout << "Element size: " << elemSize << " bytes" << std::endl;
+        std::cout << "Begin " << directionStr << " rate test at " << (sampleRate/1e6) << " Msps" << std::endl;
+        runRateTestStreamLoop(device, stream, direction, channels.size(), elemSize);
 
         //cleanup stream and device
         device->closeStream(stream);
