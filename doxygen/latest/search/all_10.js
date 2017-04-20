0ce86aec9d665a2f9ccb28782dc69b5b90cb6a64
Josh Blum <josh@joshknows.com>
2017-04-19 17:35:07 -0700

    updated latest for range and corr additions

--- doxygen/latest/search/all_10.js
+++ doxygen/latest/search/all_10.js
@@ -5,7 +5,7 @@ var searchData=
   ['time_2ehpp',['Time.hpp',['../Time_8hpp.html',1,'']]],
   ['timenstoticks',['timeNsToTicks',['../namespaceSoapySDR.html#add6d6987f643cb0605063d6f5d0fe6e5',1,'SoapySDR']]],
   ['transactspi',['transactSPI',['../classSoapySDR_1_1Device.html#a58355399bd15c0d3060944c8274e968c',1,'SoapySDR::Device']]],
-  ['type',['type',['../structSoapySDRArgInfo.html#a1a36d752e73cc4535e4cb1f5f78af812',1,'SoapySDRArgInfo::type()'],['../classSoapySDR_1_1ArgInfo.html#a1ec967f03125bafa28cdde0e31c81c2f',1,'SoapySDR::ArgInfo::type()'],['../classSoapySDR_1_1ArgInfo.html#acf2322d6142ab0ae1f137730b6b54aed',1,'SoapySDR::ArgInfo::Type()']]],
+  ['type',['Type',['../classSoapySDR_1_1ArgInfo.html#acf2322d6142ab0ae1f137730b6b54aed',1,'SoapySDR::ArgInfo::Type()'],['../structSoapySDRArgInfo.html#a1a36d752e73cc4535e4cb1f5f78af812',1,'SoapySDRArgInfo::type()'],['../classSoapySDR_1_1ArgInfo.html#a1ec967f03125bafa28cdde0e31c81c2f',1,'SoapySDR::ArgInfo::type()']]],
   ['types_2eh',['Types.h',['../Types_8h.html',1,'']]],
   ['types_2ehpp',['Types.hpp',['../Types_8hpp.html',1,'']]]
 ];
--- doxygen/latest/search/functions_6.js
+++ doxygen/latest/search/functions_6.js
@@ -2,6 +2,7 @@ var searchData=
 [
   ['hasdcoffset',['hasDCOffset',['../classSoapySDR_1_1Device.html#a7010c831f72cb26898ff8abd83057ceb',1,'SoapySDR::Device']]],
   ['hasdcoffsetmode',['hasDCOffsetMode',['../classSoapySDR_1_1Device.html#ac59df047fc98fbed1799e048af0ae730',1,'SoapySDR::Device']]],
+  ['hasfrequencycorrection',['hasFrequencyCorrection',['../classSoapySDR_1_1Device.html#aea22ee12762e2a849b384669fe7d83d6',1,'SoapySDR::Device']]],
   ['hasgainmode',['hasGainMode',['../classSoapySDR_1_1Device.html#a97c5d620f2699425a793e283eeadd2bf',1,'SoapySDR::Device']]],
   ['hashardwaretime',['hasHardwareTime',['../classSoapySDR_1_1Device.html#a77669546f4d519b589e1ba75ade18a24',1,'SoapySDR::Device']]],
   ['hasiqbalance',['hasIQBalance',['../classSoapySDR_1_1Device.html#acd491ac255fb9cc746ae4d883a551f68',1,'SoapySDR::Device']]]
