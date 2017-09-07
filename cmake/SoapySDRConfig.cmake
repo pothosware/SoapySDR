e1cd42ab551c273634b23e7ca4e6549f8f9e3f37
Coburn <coburnwightman@gmail.com>
2017-09-07 17:36:19 +0000

    enable c++11

--- cmake/SoapySDRConfig.cmake
+++ cmake/SoapySDRConfig.cmake
@@ -63,7 +63,17 @@ endif()
 ########################################################################
 # Helpful compiler flags
 ########################################################################
+
+#C++11 is a required language feature for this project
+set(CMAKE_CXX_STANDARD 11)
+
 if(CMAKE_COMPILER_IS_GNUCXX)
+
+    #enable C++11 on older versions of cmake
+    if (CMAKE_VERSION VERSION_LESS "3.1")
+      set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++0x")
+    endif()
+
     #force a compile-time error when symbols are missing
     set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -Wl,--no-undefined")
     set(CMAKE_MODULE_LINKER_FLAGS "${CMAKE_MODULE_LINKER_FLAGS} -Wl,--no-undefined")
