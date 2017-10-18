2a9c10bbfd6a4a57422ea1d312ed418af5c6b777
Coburn <coburnwightman@gmail.com>
2017-10-18 18:40:41 +0000

    add real format converters

--- lib/DefaultConverters.cpp
+++ lib/DefaultConverters.cpp
@@ -9,6 +9,338 @@
 #include <SoapySDR/Formats.hpp>
 #include <cstring> //memcpy
 
+// ********************************
+// Real Soapy Formats
+
+// Copy Converters
+
+// F32 <> F32
+static void genericF32toF32(const void *srcBuff, void *dstBuff, const size_t numElems, const double scaler)
+{
+  const size_t elemDepth = 1;
+
+  if (scaler == 1.0)
+    {
+      const size_t sampleSize = sizeof(float);
+      std::memcpy(dstBuff, srcBuff, numElems*elemDepth*sampleSize);
+    }
+  else
+    {
+      auto *src = (float*)srcBuff;
+      auto *dst = (float*)dstBuff;
+      for (size_t i = 0; i < numElems*elemDepth; i++)
+	{
+	  dst[i] = float(src[i]) * scaler;
+	}
+    }
+}
+
+static SoapySDR::ConverterRegistry registerGenericF32toF32(SOAPY_SDR_F32, SOAPY_SDR_F32, SoapySDR::ConverterRegistry::GENERIC, &genericF32toF32);
+
+// S32 <> S32
+static void genericS32toS32(const void *srcBuff, void *dstBuff, const size_t numElems, const double scaler)
+{
+  const size_t elemDepth = 1;
+
+  if (scaler == 1.0)
+    {
+      const size_t sampleSize = sizeof(int32_t);
+      std::memcpy(dstBuff, srcBuff, numElems*elemDepth*sampleSize);
+    }
+  else
+    {
+      auto *src = (int32_t*)srcBuff;
+      auto *dst = (int32_t*)dstBuff;
+      for (size_t i = 0; i < numElems*elemDepth; i++)
+	{
+	  dst[i] = int32_t(src[i]) * scaler;
+	}
+    }
+}
+
+static SoapySDR::ConverterRegistry registerGenericS32toS32(SOAPY_SDR_S32, SOAPY_SDR_S32, SoapySDR::ConverterRegistry::GENERIC, &genericS32toS32);
+
+// S16 <> S16
+static void genericS16toS16(const void *srcBuff, void *dstBuff, const size_t numElems, const double scaler)
+{
+  const size_t elemDepth = 1;
+
+  if (scaler == 1.0)
+    {
+      const size_t sampleSize = sizeof(int16_t);
+      std::memcpy(dstBuff, srcBuff, numElems*elemDepth*sampleSize);
+    }
+  else
+    {
+      auto *src = (int16_t*)srcBuff;
+      auto *dst = (int16_t*)dstBuff;
+      for (size_t i = 0; i < numElems*elemDepth; i++)
+	{
+	  dst[i] = int16_t(src[i]) * scaler;
+	}
+    }
+}
+
+static SoapySDR::ConverterRegistry registerGenericS16toS16(SOAPY_SDR_S16, SOAPY_SDR_S16, SoapySDR::ConverterRegistry::GENERIC, &genericS16toS16);
+
+// S8 <> S8
+static void genericS8toS8(const void *srcBuff, void *dstBuff, const size_t numElems, const double scaler)
+{
+  const size_t elemDepth = 1;
+
+  if (scaler == 1.0)
+    {
+      const size_t sampleSize = sizeof(int8_t);
+      std::memcpy(dstBuff, srcBuff, numElems*elemDepth*sampleSize);
+    }
+  else
+    {
+      auto *src = (int8_t*)srcBuff;
+      auto *dst = (int8_t*)dstBuff;
+      for (size_t i = 0; i < numElems*elemDepth; i++)
+	{
+	  dst[i] = int8_t(src[i]) * scaler;
+	}
+    }
+}
+
+static SoapySDR::ConverterRegistry registerGenericS8toS8(SOAPY_SDR_S8, SOAPY_SDR_S8, SoapySDR::ConverterRegistry::GENERIC, &genericS8toS8);
+
+
+// Type Converters
+
+// F32 <> S16
+static void genericF32toS16(const void *srcBuff, void *dstBuff, const size_t numElems, const double scaler)
+{
+  const size_t elemDepth = 1;
+
+  auto *src = (float*)srcBuff;
+  auto *dst = (int16_t*)dstBuff;
+  for (size_t i = 0; i < numElems*elemDepth; i++)
+    {
+      dst[i] = SoapySDR::F32toS16(src[i] * scaler);
+    }
+}
+
+static void genericS16toF32(const void *srcBuff, void *dstBuff, const size_t numElems, const double scaler)
+{
+  const size_t elemDepth = 1;
+
+  auto *src = (int16_t*)srcBuff;
+  auto *dst = (float*)dstBuff;
+  for (size_t i = 0; i < numElems*elemDepth; i++)
+    {
+      dst[i] = SoapySDR::S16toF32(src[i]) * scaler;
+    }
+}
+
+static SoapySDR::ConverterRegistry registerGenericF32toS16(SOAPY_SDR_F32, SOAPY_SDR_S16, SoapySDR::ConverterRegistry::GENERIC, &genericF32toS16);
+static SoapySDR::ConverterRegistry registerGenericS16toF32(SOAPY_SDR_S16, SOAPY_SDR_F32, SoapySDR::ConverterRegistry::GENERIC, &genericS16toF32);
+
+
+// F32 <> U16
+static void genericF32toU16(const void *srcBuff, void *dstBuff, const size_t numElems, const double scaler)
+{
+  const size_t elemDepth = 1;
+
+  auto *src = (float*)srcBuff;
+  auto *dst = (uint16_t*)dstBuff;
+  for (size_t i = 0; i < numElems*elemDepth; i++)
+    {
+      dst[i] = SoapySDR::F32toU16(src[i] * scaler);
+    }
+}
+
+static void genericU16toF32(const void *srcBuff, void *dstBuff, const size_t numElems, const double scaler)
+{
+  const size_t elemDepth = 1;
+
+  auto *src = (uint16_t*)srcBuff;
+  auto *dst = (float*)dstBuff;
+  for (size_t i = 0; i < numElems*elemDepth; i++)
+    {
+      dst[i] = SoapySDR::U16toF32(src[i]) * scaler;
+    }
+}
+
+static SoapySDR::ConverterRegistry registerGenericF32toU16(SOAPY_SDR_F32, SOAPY_SDR_U16, SoapySDR::ConverterRegistry::GENERIC, &genericF32toU16);
+static SoapySDR::ConverterRegistry registerGenericU16toF32(SOAPY_SDR_U16, SOAPY_SDR_F32, SoapySDR::ConverterRegistry::GENERIC, &genericU16toF32);
+
+
+// F32 <> S8
+static void genericF32toS8(const void *srcBuff, void *dstBuff, const size_t numElems, const double scaler)
+{
+  const size_t elemDepth = 1;
+
+  auto *src = (float*)srcBuff;
+  auto *dst = (int8_t*)dstBuff;
+  for (size_t i = 0; i < numElems*elemDepth; i++)
+    {
+      dst[i] = SoapySDR::F32toS8(src[i] * scaler);
+    }
+}
+
+static void genericS8toF32(const void *srcBuff, void *dstBuff, const size_t numElems, const double scaler)
+{
+  const size_t elemDepth = 1;
+
+  auto *src = (int8_t*)srcBuff;
+  auto *dst = (float*)dstBuff;
+  for (size_t i = 0; i < numElems*elemDepth; i++)
+    {
+      dst[i] = SoapySDR::S8toF32(src[i]) * scaler;
+    }
+}
+
+static SoapySDR::ConverterRegistry registerGenericF32toS8(SOAPY_SDR_F32, SOAPY_SDR_U8, SoapySDR::ConverterRegistry::GENERIC, &genericF32toS8);
+static SoapySDR::ConverterRegistry registerGenericS8toF32(SOAPY_SDR_U8, SOAPY_SDR_F32, SoapySDR::ConverterRegistry::GENERIC, &genericS8toF32);
+
+
+// F32 <> U8
+static void genericF32toU8(const void *srcBuff, void *dstBuff, const size_t numElems, const double scaler)
+{
+  const size_t elemDepth = 1;
+
+  auto *src = (float*)srcBuff;
+  auto *dst = (uint8_t*)dstBuff;
+  for (size_t i = 0; i < numElems*elemDepth; i++)
+    {
+      dst[i] = SoapySDR::F32toU8(src[i] * scaler);
+    }
+}
+
+static void genericU8toF32(const void *srcBuff, void *dstBuff, const size_t numElems, const double scaler)
+{
+  const size_t elemDepth = 1;
+
+  auto *src = (uint8_t*)srcBuff;
+  auto *dst = (float*)dstBuff;
+  for (size_t i = 0; i < numElems*elemDepth; i++)
+    {
+      dst[i] = SoapySDR::U8toF32(src[i]) * scaler;
+    }
+}
+
+static SoapySDR::ConverterRegistry registerGenericF32toU8(SOAPY_SDR_F32, SOAPY_SDR_S8, SoapySDR::ConverterRegistry::GENERIC, &genericF32toU8);
+static SoapySDR::ConverterRegistry registerGenericU8toF32(SOAPY_SDR_S8, SOAPY_SDR_F32, SoapySDR::ConverterRegistry::GENERIC, &genericU8toF32);
+
+// S16 <> U16
+static void genericS16toU16(const void *srcBuff, void *dstBuff, const size_t numElems, const double scaler)
+{
+  const size_t elemDepth = 1;
+
+  auto *src = (int16_t*)srcBuff;
+  auto *dst = (uint16_t*)dstBuff;
+  for (size_t i = 0; i < numElems*elemDepth; i++)
+    {
+      dst[i] = SoapySDR::S16toU16(src[i] * scaler);
+    }
+}
+
+static void genericU16toS16(const void *srcBuff, void *dstBuff, const size_t numElems, const double scaler)
+{
+  const size_t elemDepth = 1;
+
+  auto *src = (uint16_t*)srcBuff;
+  auto *dst = (int16_t*)dstBuff;
+  for (size_t i = 0; i < numElems*elemDepth; i++)
+    {
+      dst[i] = SoapySDR::U16toS16(src[i]) * scaler;
+    }
+}
+
+static SoapySDR::ConverterRegistry registerGenericS16toU16(SOAPY_SDR_S16, SOAPY_SDR_U16, SoapySDR::ConverterRegistry::GENERIC, &genericS16toU16);
+static SoapySDR::ConverterRegistry registerGenericU16toS16(SOAPY_SDR_U16, SOAPY_SDR_S16, SoapySDR::ConverterRegistry::GENERIC, &genericU16toS16);
+
+
+// S16 <> S8
+static void genericS16toS8(const void *srcBuff, void *dstBuff, const size_t numElems, const double scaler)
+{
+  const size_t elemDepth = 1;
+
+  auto *src = (int16_t*)srcBuff;
+  auto *dst = (int8_t*)dstBuff;
+  for (size_t i = 0; i < numElems*elemDepth; i++)
+    {
+      dst[i] = SoapySDR::S16toS8(src[i] * scaler);
+    }
+}
+
+static void genericS8toS16(const void *srcBuff, void *dstBuff, const size_t numElems, const double scaler)
+{
+  const size_t elemDepth = 1;
+
+  auto *src = (int8_t*)srcBuff;
+  auto *dst = (int16_t*)dstBuff;
+  for (size_t i = 0; i < numElems*elemDepth; i++)
+    {
+      dst[i] = SoapySDR::S8toS16(src[i]) * scaler;
+    }
+}
+
+static SoapySDR::ConverterRegistry registerGenericS16toS8(SOAPY_SDR_S16, SOAPY_SDR_S8, SoapySDR::ConverterRegistry::GENERIC, &genericS16toS8);
+static SoapySDR::ConverterRegistry registerGenericS8toS16(SOAPY_SDR_S8, SOAPY_SDR_S16, SoapySDR::ConverterRegistry::GENERIC, &genericS8toS16);
+
+// S16 <> U8
+static void genericS16toU8(const void *srcBuff, void *dstBuff, const size_t numElems, const double scaler)
+{
+  const size_t elemDepth = 1;
+
+  auto *src = (int16_t*)srcBuff;
+  auto *dst = (uint8_t*)dstBuff;
+  for (size_t i = 0; i < numElems*elemDepth; i++)
+    {
+      dst[i] = SoapySDR::S16toU8(src[i] * scaler);
+    }
+}
+
+static void genericU8toS16(const void *srcBuff, void *dstBuff, const size_t numElems, const double scaler)
+{
+  const size_t elemDepth = 1;
+
+  auto *src = (uint8_t*)srcBuff;
+  auto *dst = (int16_t*)dstBuff;
+  for (size_t i = 0; i < numElems*elemDepth; i++)
+    {
+      dst[i] = SoapySDR::U8toS16(src[i]) * scaler;
+    }
+}
+
+static SoapySDR::ConverterRegistry registerGenericS16toU8(SOAPY_SDR_S16, SOAPY_SDR_U8, SoapySDR::ConverterRegistry::GENERIC, &genericS16toU8);
+static SoapySDR::ConverterRegistry registerGenericU8toS16(SOAPY_SDR_U8, SOAPY_SDR_S16, SoapySDR::ConverterRegistry::GENERIC, &genericU8toS16);
+
+// U16 <> S8
+static void genericU16toS8(const void *srcBuff, void *dstBuff, const size_t numElems, const double scaler)
+{
+  const size_t elemDepth = 1;
+
+  auto *src = (uint16_t*)srcBuff;
+  auto *dst = (int8_t*)dstBuff;
+  for (size_t i = 0; i < numElems*elemDepth; i++)
+    {
+      dst[i] = SoapySDR::U16toS8(src[i] * scaler);
+    }
+}
+
+static void genericS8toU16(const void *srcBuff, void *dstBuff, const size_t numElems, const double scaler)
+{
+  const size_t elemDepth = 1;
+
+  auto *src = (int8_t*)srcBuff;
+  auto *dst = (uint16_t*)dstBuff;
+  for (size_t i = 0; i < numElems*elemDepth; i++)
+    {
+      dst[i] = SoapySDR::S8toU16(src[i]) * scaler;
+    }
+}
+
+static SoapySDR::ConverterRegistry registerGenericU16toS8(SOAPY_SDR_U16, SOAPY_SDR_S8, SoapySDR::ConverterRegistry::GENERIC, &genericU16toS8);
+static SoapySDR::ConverterRegistry registerGenericS8toU16(SOAPY_SDR_S8, SOAPY_SDR_U16, SoapySDR::ConverterRegistry::GENERIC, &genericS8toU16);
+
+
+// ********************************
+// Complex Data Types
+
 // Copy Converters
 
 // CF32 <> CF32
@@ -18,8 +350,8 @@ static void genericCF32toCF32(const void *srcBuff, void *dstBuff, const size_t n
 
   if (scaler == 1.0)
     {
-      const size_t elemSize = sizeof(float);
-      std::memcpy(dstBuff, srcBuff, numElems*elemSize*elemDepth);
+      const size_t sampleSize = sizeof(float);
+      std::memcpy(dstBuff, srcBuff, numElems*elemDepth*sampleSize);
     }
   else
     {
@@ -41,8 +373,8 @@ static void genericCS32toCS32(const void *srcBuff, void *dstBuff, const size_t n
 
   if (scaler == 1.0)
     {
-      const size_t elemSize = sizeof(int32_t);
-      std::memcpy(dstBuff, srcBuff, numElems*elemSize*elemDepth);
+      const size_t sampleSize = sizeof(int32_t);
+      std::memcpy(dstBuff, srcBuff, numElems*elemDepth*sampleSize);
     }
   else
     {
@@ -64,8 +396,8 @@ static void genericCS16toCS16(const void *srcBuff, void *dstBuff, const size_t n
 
   if (scaler == 1.0)
     {
-      const size_t elemSize = sizeof(int16_t);
-      std::memcpy(dstBuff, srcBuff, numElems*elemSize*elemDepth);
+      const size_t sampleSize = sizeof(int16_t);
+      std::memcpy(dstBuff, srcBuff, numElems*elemDepth*sampleSize);
     }
   else
     {
@@ -87,8 +419,8 @@ static void genericCS8toCS8(const void *srcBuff, void *dstBuff, const size_t num
 
   if (scaler == 1.0)
     {
-      const size_t elemSize = sizeof(int8_t);
-      std::memcpy(dstBuff, srcBuff, numElems*elemSize*elemDepth);
+      const size_t sampleSize = sizeof(int8_t);
+      std::memcpy(dstBuff, srcBuff, numElems*elemDepth*sampleSize);
     }
   else
     {
