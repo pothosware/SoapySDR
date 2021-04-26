f1dc37aa33b69e7361dd845986537c4f0ae1a704
Josh Blum <josh@joshknows.com>
2021-04-25 23:03:19 -0500

    0.8.0

---
+++ doxygen/0.8.0/search/classes_5.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['soapysdrarginfo_512',['SoapySDRArgInfo',['../structSoapySDRArgInfo.html',1,'']]],
+  ['soapysdrkwargs_513',['SoapySDRKwargs',['../structSoapySDRKwargs.html',1,'']]],
+  ['soapysdrrange_514',['SoapySDRRange',['../structSoapySDRRange.html',1,'']]]
+];
