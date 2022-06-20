ba1caa846c181e94f244f3d468285e1ab7c528a6
Nicholas Corgan <n.corgan@gmail.com>
2022-06-20 09:15:51 -0500

    Enum naming convention

--- swig/java/swig/Constants.hpp
+++ swig/java/swig/Constants.hpp
@@ -17,36 +17,36 @@ namespace SoapySDR { namespace Java {
     //       SWIG will copy SOAPY_SDR* into the Java file.
     enum class Direction
     {
-        Tx = 0,
-        Rx = 1
+        TX = 0,
+        RX = 1
     };
 
     // Note: we need to repeat the literal enum values or
     //       SWIG will copy SOAPY_SDR* into the Java file.
     enum class ErrorCode
     {
-        None         = 0,
-        Timeout      = -1,
-        StreamError  = -2,
-        Corruption   = -3,
-        Overflow     = -4,
-        NotSupported = -5,
-        TimeError    = -6,
-        Underflow    = -7
+        NONE          = 0,
+        TIMEOUT       = -1,
+        STREAM_ERROR  = -2,
+        CORRUPTION    = -3,
+        OVERFLOW      = -4,
+        NOT_SUPPORTED = -5,
+        TIME_ERROR    = -6,
+        UNDERFLOW     = -7
     };
 
     // Note: we need to repeat the literal enum values or
     //       SWIG will copy SOAPY_SDR* into the Java file.
     enum class LogLevel
     {
-        Fatal    = 1,
-        Critical = 2,
-        Error    = 3,
-        Warning  = 4,
-        Notice   = 5,
-        Info     = 6,
-        Debug    = 7,
-        Trace    = 8,
+        FATAL    = 1,
+        CRITICAL = 2,
+        ERROR    = 3,
+        WARNING  = 4,
+        NOTICE   = 5,
+        INFO     = 6,
+        DEBUG    = 7,
+        TRACE    = 8,
         SSI      = 9
     };
 }}
@@ -56,3 +56,22 @@ namespace SoapySDR { namespace Java {
 // enforce equality with these static_asserts.
 #define ENUM_CHECK(_enum,_define) \
     static_assert(int(_enum) == _define, #_define)
+
+ENUM_CHECK(SoapySDR::Java::Direction::TX, SOAPY_SDR_TX);
+ENUM_CHECK(SoapySDR::Java::Direction::RX, SOAPY_SDR_RX);
+ENUM_CHECK(SoapySDR::Java::ErrorCode::TIMEOUT, SOAPY_SDR_TIMEOUT);
+ENUM_CHECK(SoapySDR::Java::ErrorCode::STREAM_ERROR, SOAPY_SDR_STREAM_ERROR);
+ENUM_CHECK(SoapySDR::Java::ErrorCode::CORRUPTION, SOAPY_SDR_CORRUPTION);
+ENUM_CHECK(SoapySDR::Java::ErrorCode::OVERFLOW, SOAPY_SDR_OVERFLOW);
+ENUM_CHECK(SoapySDR::Java::ErrorCode::NOT_SUPPORTED, SOAPY_SDR_NOT_SUPPORTED);
+ENUM_CHECK(SoapySDR::Java::ErrorCode::TIME_ERROR, SOAPY_SDR_TIME_ERROR);
+ENUM_CHECK(SoapySDR::Java::ErrorCode::UNDERFLOW, SOAPY_SDR_UNDERFLOW);
+ENUM_CHECK(SoapySDR::Java::LogLevel::FATAL, SOAPY_SDR_FATAL);
+ENUM_CHECK(SoapySDR::Java::LogLevel::CRITICAL, SOAPY_SDR_CRITICAL);
+ENUM_CHECK(SoapySDR::Java::LogLevel::ERROR, SOAPY_SDR_ERROR);
+ENUM_CHECK(SoapySDR::Java::LogLevel::WARNING, SOAPY_SDR_WARNING);
+ENUM_CHECK(SoapySDR::Java::LogLevel::NOTICE, SOAPY_SDR_NOTICE);
+ENUM_CHECK(SoapySDR::Java::LogLevel::INFO, SOAPY_SDR_INFO);
+ENUM_CHECK(SoapySDR::Java::LogLevel::DEBUG, SOAPY_SDR_DEBUG);
+ENUM_CHECK(SoapySDR::Java::LogLevel::TRACE, SOAPY_SDR_TRACE);
+ENUM_CHECK(SoapySDR::Java::LogLevel::SSI, SOAPY_SDR_SSI);
