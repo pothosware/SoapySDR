8c6cb7c5223fad995e355486527589c63aa3b21e
Josh Blum <josh@joshknows.com>
2024-04-24 07:35:28 -0500

    use old get python lib for v2 (#437)
    * cmake: use original get python lib for v2
    * ci: no python2 in the windows matrix
    * ci: remove python2 from osx
    * ci: remove freebsd (would not build)

---
+++ swig/python/get_python2_lib.py
@@ -0,0 +1,19 @@
+import os
+import sys
+import site
+from distutils.sysconfig import get_python_lib
+
+if __name__ == '__main__':
+    prefix = sys.argv[1]
+
+    #ask distutils where to install the python module
+    install_dir = get_python_lib(plat_specific=True, prefix=prefix)
+
+    #use sites when the prefix is already recognized
+    try:
+        paths = [p for p in site.getsitepackages() if p.startswith(prefix)]
+        if len(paths) == 1: install_dir = paths[0]
+    except AttributeError: pass
+
+    #strip the prefix to return a relative path
+    print(os.path.relpath(install_dir, prefix))
